1741b35a9a623152a9050923cf6a88476299658c
Feh Foo Bar <feh@cock.li>
2014-09-01 10:29:16 -0300

    Move JS to 2 different files

---
+++ js/game/main.js
@@ -0,0 +1,263 @@
+var game = {};
+function init() {
+	_.id = _.identity;
+	game.buttonAction = "firstDay";
+	hide("#storage");
+	hide("#progress");
+	$(".hidden").hide();
+	var aux = function () {
+		eval(game.buttonAction)();
+	};
+	$("#button").button().click(aux);
+	$(document).bind('keyup', 'return', aux);
+	$(document).bind('keyup', 'F4', resetSave);
+	$("#reset").button().click(resetSave);
+	hide("#accordion").accordion();
+	$("#accordion > div").css({
+		"height" : "auto",
+		"padding" : "0px"
+	});
+	_.each(["morning", "afternoon", "evening", "night"], genRadioSet);
+	unpressButtons();
+	$("form").css({margin:"0px"});
+	game.dayTime = true;
+	game.disabledDayTime = null;
+	game.storage = {};
+	game.triggers = [];
+	game.day = 1;
+	game.tasks = {};
+	game.afterToggle = ["_.id", null];
+	game.activities = {};
+	game.activity = {
+		"morning" : null,
+		"afternoon" : null,
+		"evening" : null,
+		"night" : null
+	};
+	restoreSave();
+	game.radios = [];
+	setActivities();
+	setTriggers();
+}
+
+
+function restoreSave() {
+	game = getLocalStorage("game", game);
+	if(_.size(game.storage) > 0) {
+		toggleDayTime();
+		eval(game.afterToggle[0])(game.afterToggle[1]);
+		if(game.dayTime) startNewDay();
+		fadeIn("#storage");
+		$("#dialog").empty();
+		$("#button").hide();
+		setTriggers();
+		game.dayTime = !game.dayTime;
+		triggerEvents();
+		game.dayTime = !game.dayTime;
+		disableDayTime();
+	}
+	if(_.size(game.tasks) > 0) {
+		fadeIn("#progress");
+		if(game.patchedRoof) {
+			$("#roof").parent().hide();
+		}
+	}
+}
+
+function setActivities() {
+	_.each(game.activities, function (attr, text) {
+		addActivity(text, attr[0], attr[1]);
+	});
+}
+
+function resetSave() {
+	setLocalStorage("game", null);
+	document.location.reload();
+}
+
+function trigger(condition, action) {
+	this.condition = condition;
+	this.action = action;
+}
+
+function firstDay() {
+	$("#button").fadeOut(400, startDay);
+	game.buttonAction = "_.id";
+}
+
+function startDay() {
+	triggerEvents();
+	toggleDayTime();
+}
+
+function searchFood() {
+	changeStorageBy("food", 2);
+	appendMessage("#findFood");
+}
+
+function collectWood() {
+	changeStorageBy("wood", 3);
+	appendMessage("#collectWood");
+}
+
+function changeStorageBy(item, amount) {
+	game.storage[item] = amount + (game.storage[item] | 0);
+	game.storage[item] = (game.storage[item] > 0) ? game.storage[item] : 0; 
+}
+
+function updateGame() {
+	if(_.size(_.filter(game.activity, function (v) {
+		return v != null;
+		})) != 2) {
+		return;
+	}
+	fadeOut("#accordion", function () {
+		commitActions();
+		enableDayTime();
+		setLocalStorage("game", game);
+		triggerEvents();
+		game.radios = [];
+		toggleDayTime();
+		disableDayTime();
+		if(game.dayTime) startNewDay();
+	});
+}
+
+function triggerEvents() {
+	_.each(game.triggers, function (t) {
+		if(t.condition()) t.action();
+	});
+}
+
+function unpressButtons(){
+	$(".ui-state-active").removeClass("ui-state-active");
+}
+
+function startNewDay() {
+	game.day++;
+	unpressButtons();
+}
+
+function commitActions() {
+	_.each(game.activity, function (f, t) {
+		if(f != null) eval(f)();
+		game.activity[t] = null;
+	});
+}
+
+function fixRoof() {
+	show("#progress");
+	game.tasks.roof = 20 + (game.tasks.roof | 0);
+	changeStorageBy("wood", -1);
+	$("#roof").html(game.tasks.roof + "%");
+}
+
+function addActivity(text, action, time) {
+	var i = _.size(game.radios);
+	_.each(time, function (t) {
+		var radio = $("#radio" + t + i);
+		radio.button({label: text}).click({
+			time:t,
+			action:action},
+			selectActivity);
+		radio.parent().show();
+	});
+	game.radios.push(action);
+	game.activities[text] = [action, time];
+}
+
+function removeActivity(text) {
+	$(":radio").map(function (i, v) {
+		var b = $(v)
+		if(b.button('option', 'label') == text) {
+			b.button("option", "label", "");
+			b.parent().hide();
+			b.click(_.id);
+		}
+	});
+	game.activities[text] = undefined;
+}
+
+function selectActivity(ev) {
+	game.activity[ev.data.time] = ev.data.action;
+	updateGame();
+}
+function genRadioSet(time) {
+	$("#" + time).append("<form><div id='radioset" + time + "'>");
+	_.each(_.range(5), function (v) {
+		var container = $("<div>");
+		$("#radioset" + time).append(container)
+		container.append("<input type='radio' id='radio" + time + v + "' name='radio'" + time + "'>")
+		container.append("<label for='radio" + time + v + "'>");
+	});
+	$("#radioset" + time).buttonset();
+	$("#radioset" + time + " > div").hide();
+}
+function appendMessage(id) {
+	$("#dialog").prepend($(id).clone());
+	$(id).fadeIn();
+}
+
+function toggleDayTime() {
+	if(game.dayTime){
+		$(".morning,.afternoon").show();
+		$(".evening,.night").hide();
+		game.activity["morning"] = null;
+		game.activity["afternoon"] = null;
+	}
+	else {
+		$(".morning,.afternoon").hide();
+		$(".evening,.night").show();
+		game.activity["evening"] = null;
+		game.activity["night"] = null;
+	}
+	game.dayTime = ! game.dayTime;
+	fadeIn("#accordion");
+	updateStorage();
+}
+
+function updateStorage() {
+	$("#wood").html(game.storage.wood);
+	$("#food").html(game.storage.food);
+}
+
+function enableDayTime() {
+	if(game.disabledDayTime != null) {
+		$("#radioset" + game.disabledDayTime).buttonset({disabled:false})
+		game.disabledDayTime = null;
+	}
+}
+
+function disableDayTime() {
+	if(game.disabledDayTime != null) {
+		$("#radioset" + game.disabledDayTime).buttonset({disabled:true})
+		game.activity[game.disabledDayTime] = "_.id";
+	}
+}
+
+/*Fade with no display:none*/
+function fadeIn(id, fun) {
+	return $(id).fadeTo("normal", 1, fun);
+}
+function fadeOut(id, fun) {
+	return $(id).fadeTo("normal", 0, fun);
+}
+function show(id) {
+	return $(id).fadeTo(0, 1);
+}
+function hide(id) {
+	return $(id).fadeTo(0, 0);
+}
+function getLocalStorage(item, default_) {
+	var value = JSON.parse(localStorage.getItem(item));
+	if(value == null) {
+		return default_;
+    }
+    else {
+        return value;
+    }
+}
+
+function setLocalStorage(item, value) {
+    localStorage.setItem(item, JSON.stringify(value));
+}
---
+++ js/game/triggers.js
@@ -0,0 +1,82 @@
+function setTriggers() {
+	game.triggers = [
+new trigger(
+	function () {
+		return game.dayTime;
+	}, function () {
+		appendMessage("<div class='day-counter'>Day: " + game.day + "</div><hr/>");
+	}
+), new trigger(
+	function () {
+		return game.day == 1 && game.dayTime == false;
+	}, function () {
+		appendMessage("#day1evening");
+	}
+), new trigger(
+	function () {
+		return game.day == 1 && game.dayTime == true;
+	}, function () {
+		appendMessage("#day1morning");
+		addActivity("Find food", "searchFood", ["morning", "afternoon"]);
+		addActivity("Collect wood", "collectWood", ["morning", "afternoon"]);
+		addActivity("Sleep", "_.id", ["evening", "night"]);
+		fadeIn("#storage");
+	}
+), new trigger(
+	function () {
+		return game.day > 1 && game.dayTime == true;
+	}, function () {
+		if(game.storage.food > 0) {
+			changeStorageBy("food", -1);
+			appendMessage("#eatBreakfast");
+		}
+		else {
+			appendMessage("#starving");
+			game.disabledDayTime = "morning";
+		}
+	}
+), new trigger(
+	function () {
+		return game.day > 2 && _.random(0, 100) < 10 && (!game.patchedRoof);
+	}, function () {
+		var wood = _.random(1, 3);
+		var food = _.random(1, 3);
+		changeStorageBy("wood", -wood);
+		changeStorageBy("food", -food);
+		appendMessage("<div>" + wood + " wood and " + food + " food units were washed away by the rain.</div>");
+		appendMessage("#rain");
+	}
+), new trigger(
+	function () {
+		return game.tasks.roof == 100 && (! game.patchedRoof);
+	}, function () {
+		appendMessage("#fixRoofComplete");
+		game.patchedRoof = true;
+		removeActivity("Fix roof");
+		$("#roof").parent().fadeOut();
+		appendMessage("#gameOver");
+	}
+), new trigger(
+	function () {
+		return game.day > 0 && game.dayTime == false;
+	}, function () {
+		if(game.storage.wood > 0) {
+			changeStorageBy("wood", -1);
+			appendMessage("#campfire");
+		}
+		else {
+			appendMessage("#freezing");
+			game.disabledDayTime = "evening";
+		}
+	}
+), new trigger(
+	function () {
+		return game.storage.wood > 5 && game.tasks.roof == undefined;
+	}, function () {
+		appendMessage("#fixRoof1");
+		addActivity("Fix roof", "fixRoof", ["evening"]);
+		fadeIn("#progress");
+		game.tasks.roof = 0;
+	}
+)];
+}
